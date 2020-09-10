07d9aaa0e9ac82de4b86165f12cd6f6357e9fef5
Yenaled <delaneyk.sullivan@gmail.com>
2020-09-10 08:44:12 -0700

    Update ccle_normalize.r

--- ccle/ccle_normalize.r
+++ ccle/ccle_normalize.r
@@ -8,6 +8,7 @@ output_file <- args[2]
 
 data <- read.table(input_file, sep="\t", header=TRUE, stringsAsFactors=FALSE, check.names=FALSE)
 normalization_factors <- norm_factors(data[,3:ncol(data)])
-data_norm <- as.data.frame(t(t(data[,3:ncol(data)]) / normalization_factors))
+data_norm <- t(t(data[,3:ncol(data)]) / normalization_factors)
+data_norm <- as.data.frame(log2(data_norm+1))
 data_norm <- cbind(data[,c(1,2)], data_norm)
 write.table(data_norm, file=output_file, sep="\t", quote=FALSE, row.names=FALSE)
